44c3a47dd7fb8481b79459d4c15783a1326e985e
Apollo <isguohr@qq.com>
2023-08-29 16:31:03 +0800

    修改notify样式和时间

--- plugins/notify.lua
+++ plugins/notify.lua
@@ -2,7 +2,9 @@ return {
   "rcarriga/nvim-notify",
   opts = function(_, opts)
     local newOpt = {
-      timeout = 0,
+      timeout = 6000,
+      render = "compact",
+      stages = "static",
     }
     if vim.g.transparent_background then newOpt.background_colour = "#0f1117" end
     return require("astronvim.utils").extend_tbl(opts, newOpt)
